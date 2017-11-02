992895dc3c7afce18e0d35f5f69099a3d39cf5d0
Andreas Mueller <amueller@nyu.edu>
2017-11-01 18:25:23 -0700

    replace vega10 by tab10, use tab10 in tsne exercise

--- notebooks/solutions/digits_tsne.py
+++ notebooks/solutions/digits_tsne.py
@@ -2,4 +2,4 @@ from sklearn.manifold import TSNE
 tsne = TSNE()
 X_tsne = tsne.fit_transform(X)
 plt.figure()
-plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=y)
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=plt.cm.tab10(y))
