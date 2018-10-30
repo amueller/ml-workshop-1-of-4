ca3bd36460b309dd6dce3cb625b38fe4b2aefd89
Andreas Mueller <andreas.mueller@columbia.edu>
2018-10-30 19:52:45 -0400

    removed unused solutions

--- notebooks/solutions/cross_validation_iris.py
+++
@@ -1,8 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import StratifiedKFold, KFold, cross_val_score
-from sklearn.svm import LinearSVC
-iris = load_iris()
-X, y = iris.data, iris.target
-
-print(cross_val_score(LinearSVC(), X, y, cv=KFold(len(X), 3)))
-print(cross_val_score(LinearSVC(), X, y, cv=StratifiedKFold(y, 3)))
--- notebooks/solutions/digits_tsne.py
+++
@@ -1,5 +0,0 @@
-from sklearn.manifold import TSNE
-tsne = TSNE()
-X_tsne = tsne.fit_transform(X)
-plt.figure()
-plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=plt.cm.tab10(y))
