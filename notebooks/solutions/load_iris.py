171e832eeff74b50f2985e628c2d9c6c7da719ec
Andreas Mueller <andreas.mueller@columbia.edu>
2018-10-30 19:51:04 -0400

    add csv loading to solution for 01

--- notebooks/solutions/load_iris.py
+++ notebooks/solutions/load_iris.py
@@ -13,5 +13,20 @@ print("Dataset size: %d  number of features: %d  number of classes: %d"
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+
 plt.figure()
 plt.scatter(X_train[:, 2], X_train[:, 3], c=y_train)
+plt.xlabel(iris.feature_names[2])
+plt.ylabel(iris.feature_names[3])
+
+import sklearn.datasets
+import os
+import pandas as pd
+iris_path = os.path.join(sklearn.datasets.__path__[0], 'data', 'iris.csv')
+iris_df =  pd.read_csv(iris_path, header=None)
+display(iris.head())
+
+iris_df = pd.read_csv(iris_path, skiprows=1, header=None)
+display(iris.head())
