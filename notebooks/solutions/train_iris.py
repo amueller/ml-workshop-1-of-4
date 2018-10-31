2b99d6db9801e23dab43132a5efd401f1e6330e4
Andreas Mueller <andreas.mueller@columbia.edu>
2018-10-30 18:41:31 -0700

    fix in trivial dataset construction

--- notebooks/solutions/train_iris.py
+++ notebooks/solutions/train_iris.py
@@ -24,15 +24,17 @@ rf.score(X_test, y_test)
 
 # Perfect classification (accuracy=1) on easy dataset
 from sklearn.linear_model import LogisticRegression
+X = np.random.uniform(size=(1000, 3))
+X[::2] += 1000
 y = X[:, 0] > 500
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
-logreg.score(X_test, y_test)
+print("score on trivial data: ", logreg.score(X_test, y_test))
 
 # Random classification (accuracy=.5) on random data
 y = np.random.normal(size=1000) > .0
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
-logreg.score(X_test, y_test)
+print("score on random data: ", logreg.score(X_test, y_test))
